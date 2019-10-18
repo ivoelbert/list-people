e39f2408258740e11166f0cf658a5dffcccd66f0
ivoelbert <ivo.elbert1@gmail.com>
2019-10-18 10:54:21 -0300

    Set up theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useTheme } from './hooks/useTheme';
+import './App.scss';
 
 const App: React.FC = () => {
+    const { themed } = useTheme();
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className={themed('app-container')}>
+            FIND PEOPLE!
         </div>
     );
 };
---
+++ src/context/theme.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+export type Theme = 'dark' | 'light';
+
+export interface ThemeContext {
+    theme: Theme;
+    setTheme: (theme: Theme) => void;
+    toggleTheme: () => void;
+    themed: (classes: string) => string;
+}
+
+// Default context, we won't reach this point cause this context will be provided at App level
+const voidContext: ThemeContext = {
+    theme: 'light',
+    setTheme: theme => {},
+    toggleTheme: () => {},
+    themed: classes => `${classes} light`,
+};
+
+export const ThemeContextValue = React.createContext<ThemeContext>(voidContext);
+
+export const ThemeProvider: React.FC = ({ children }) => {
+    const [theme, setTheme] = useState<Theme>('dark');
+
+    const toggleTheme = (): void => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    };
+
+    const themed = (classes: string) => {
+        return `${classes} ${theme}`;
+    };
+
+    const themeHandler: ThemeContext = {
+        theme,
+        setTheme,
+        toggleTheme,
+        themed,
+    };
+
+    return (
+        <ThemeContextValue.Provider value={themeHandler}>
+            {children}
+        </ThemeContextValue.Provider>
+    );
+};
