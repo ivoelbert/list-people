bbcede1c475c218705283eee717910af0366965b
ivoelbert <ivo.elbert1@gmail.com>
2019-10-19 17:09:11 -0300

    Updates

---
+++ precache-manifest.7166fe5b0f8f44d2c6749dc74aa79131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfeabac64503f6941d5d7a142bcdca19",
+    "url": "/list-people/index.html"
+  },
+  {
+    "revision": "cc09c428f8716bca9b1b",
+    "url": "/list-people/static/css/main.b369377d.chunk.css"
+  },
+  {
+    "revision": "1a68fe32223efaff087a",
+    "url": "/list-people/static/js/2.208b7e4e.chunk.js"
+  },
+  {
+    "revision": "cc09c428f8716bca9b1b",
+    "url": "/list-people/static/js/main.5aa1c5db.chunk.js"
+  },
+  {
+    "revision": "9699a3e2d45aa5c4ccd7",
+    "url": "/list-people/static/js/runtime-main.91d82f26.js"
+  }
+]);
