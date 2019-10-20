30eaf66c210d067299b193174799ccb367d9d62e
ivoelbert <ivo.elbert1@gmail.com>
2019-10-20 13:33:06 -0300

    Updates

---
+++ precache-manifest.4accb49da68d21c7beab22345374ffb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3810d39b56c1dddabd53d1b433a77c4",
+    "url": "/list-people/index.html"
+  },
+  {
+    "revision": "546abc48045d26130497",
+    "url": "/list-people/static/css/main.952d7d62.chunk.css"
+  },
+  {
+    "revision": "bc1fbe1ddbb79d8f53f4",
+    "url": "/list-people/static/js/2.c784fe82.chunk.js"
+  },
+  {
+    "revision": "546abc48045d26130497",
+    "url": "/list-people/static/js/main.782f41ac.chunk.js"
+  },
+  {
+    "revision": "9699a3e2d45aa5c4ccd7",
+    "url": "/list-people/static/js/runtime-main.91d82f26.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/list-people/precache-manifest.fe443b61f9f67da5343298b12c59b914.js"
+  "/list-people/precache-manifest.4accb49da68d21c7beab22345374ffb2.js"
 );
 
 self.addEventListener('message', (event) => {
